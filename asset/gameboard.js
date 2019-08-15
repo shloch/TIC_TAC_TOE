9b86cdf031736e76a9e636f4f738f8ec5415f3cb
TERVER AOSU <terveraosu@gmail.com>
2019-08-15 18:57:40 +0100

    added a div with class shade

--- asset/gameboard.js
+++ asset/gameboard.js
@@ -8,6 +8,7 @@ var GameModule = (function() {
         if (isWinner) {
             mod.alert(`GAME OVER !!! --- ${player.getName()} WON`);
             document.querySelector(".splash").innerHTML = `${player.getName()} WON`;
+            document.querySelector('div').classList.add('shade')
         } else if (gameboard.length == 9) {
             mod.alert('GAME OVER !!! --- DRAW GAME');
         };
