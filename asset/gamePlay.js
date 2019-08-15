9ac8e6755c15e2d1333606d7868471bdd333003a
TERVER AOSU <terveraosu@gmail.com>
2019-08-15 11:15:56 +0100

    game flow logi

--- asset/gamePlay.js
+++ asset/gamePlay.js
@@ -1,14 +1,15 @@
 "use strict";
 
 const playerFactory = (name, playLetter = "X") => {
-    const cellpositions = [];
+    const cell = [,,,,,,,,];
     const playPositions = (position) =>{
-        cellpositions.push(position)
+        cell.push(position)
     };
-    return { name, cellpositions, playPositions, playLetter };
+    return { name, cell, playPositions, playLetter };
 };
 
 const startgame = document.querySelector("form");
+
 startgame.addEventListener("submit", e => {
     const p1 = document.querySelector('[name="player1"]').value;
     const p2 = document.querySelector('[name="player2"]').value;
@@ -16,19 +17,36 @@ startgame.addEventListener("submit", e => {
     let player1 = playerFactory(p1, "X");
     let player2 = playerFactory(p2, "O");
 
-    player1.playPositions(2);
-    console.log(player1.cellpositions)
-    player1.playPositions(6);
-    console.log(player1.cellpositions)
-
+    player1.cell.push(2);
+    player2.cell.push(6);
+    console.log(player1,player2)
+    playLoop(player1, player2);
     e.preventDefault();
 });
 
-const playLoop = () => {
-    let playcount = 0
-    while (playcount < 9) {
-        
-        playcount++
-    }
-
+const playLoop = (player1, player2) => {
+    const fields = document.querySelectorAll("td")
+    let x = 1;
+    let fieldIndex = []
+    fields.forEach((field, index, fields) => {
+        field.addEventListener('click', event => {            
+            console.log(index);
+            if (!fieldIndex.includes(index)){
+                if (x % 2 === 0)
+                {
+                    field.innerHTML = "O";
+                    x += 1;
+                    player1.cell.push()
+                    fieldIndex.push(index)
+                }
+                else
+                {
+                    field.innerHTML = "X"
+                    x += 1;
+                    fieldIndex.push(index)
+                }    
+            }
+            console.log(x);
+        }, false)
+    })
 }
